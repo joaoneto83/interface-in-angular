4b2886e9cf023fa44564bc3d3efd811fc0f42e14
Pedro Vinicius Rodrigues Furlan <pedro.furlan@elgin.com.br>
2020-10-30 17:21:20 -0300

    Finalização parcial - alterar senha , ajax post funcional

--- src/app/main/dashboard/user-profile/recover-password/recover-password.service.ts
+++ src/app/main/dashboard/user-profile/recover-password/recover-password.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { Usuario } from 'src/app/_shered/model/usuario';
 import { environment } from 'src/environments/environment';
 import { AtualizarSenha } from 'src/shared/models/AtualizarSenhaModel';
 
@@ -10,17 +9,19 @@ export class RecoverPasswordService {
 
   constructor(private http:HttpClient) { }
 
+  
+  RecuperarSenha(senhaAtualizada:AtualizarSenha, token:string) : Observable<any> {
+    
+    // Declara cabeçalho requisicao
+    const HttpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }),
+      responseType: 'json' as const 
+    };
 
-  RecuperarSenha(senhaAtualizada:AtualizarSenha, token:string){
-    const reqHeader = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-
-    const options = { headers : reqHeader };
-
-    return this.http.post(
-    `${environment.PORTAL_API}/Login/AtualizarSenha`,
-    senhaAtualizada , options );
+    // Manda requisição POST
+    return this.http.post<Observable<any>>(`${environment.PORTAL_API}/Login/AlterarSenha`, senhaAtualizada  , HttpOptions); 
   }
 }
