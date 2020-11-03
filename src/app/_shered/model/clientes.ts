fc612022ff1cd10317de7c69a23a2f3a73865c90
João Pereira do Nascimento Neto <joaoneto@elgin.com.br>
2020-11-03 11:29:59 -0300

    atulizarção semanal

---
+++ src/app/_shered/model/clientes.ts
@@ -0,0 +1,15 @@
+// import { Dependente } from './dependente';
+// import { Arquivo } from '../../_models/Arquivo';
+
+export interface Usuario {
+      id: string,
+      nome: string,
+      email: string,
+      avatar: string,
+      perfilId: number,
+      perfil: string,
+     
+
+    totalRegistros: null
+
+}; 
