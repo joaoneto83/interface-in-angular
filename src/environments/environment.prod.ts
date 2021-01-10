8d471b60eabbfa27ecbe7c518b3332466157af1e
João Pereira do Nascimento Neto <joaoneto83@gmail.com>
2021-01-10 12:37:03 -0300

    adjustment for git

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PORTAL_API: 'http://10.20.100.61:8030/api',
+  PORTAL_API: 'https://localhost:44382/api',
   FERIADOS_API: 'https://api.calendario.com.br/?json=true&token=dmZpbmF0aUBlcnNvZnQuY29tLmJyJmhhc2g9ODg4MjE5MDE&ano='
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-
 
 export const environment = {
   production: true,
-
-PORTAL_API: 'http://10.20.100.61:8030/api',
-FERIADOS_API: 'https://api.calendario.com.br/?json=true&token=dmZpbmF0aUBlcnNvZnQuY29tLmJyJmhhc2g9ODg4MjE5MDE&ano='
+  PORTAL_API: 'https://localhost:44382/api',
+  FERIADOS_API: 'https://api.calendario.com.br/?json=true&token=dmZpbmF0aUBlcnNvZnQuY29tLmJyJmhhc2g9ODg4MjE5MDE&ano='
 };
